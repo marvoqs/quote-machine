b31426a21c53df71d62a2a2fe67c3807a10ed9b1
marvoqs <marvoqs@gmail.com>
2020-11-18 16:18:13 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const quotes = [
+  {
+    id: 1,
+    text: 'You are who you are, and no one can change that. So be yourself, because no one can do it better.',
+    author: 'Meylin D. Bojorge',
+  },
+  {
+    id: 2,
+    text: 'I hear many condemn these men because they were so few. When were the good and the braver every in a majority.',
+    author: 'Henry David Thoreau',
+  },
+  {
+    id: 3,
+    text: 'And so, my fellow Americans, ask not what your country can do for you -- ask what you can do for your country.',
+    author: 'John F. Kennedy',
+  },
+  {
+    id: 4,
+    text: 'Too many pieces of music finish too long after the end.',
+    author: 'Igor Fyodorovich Stravinsky',
+  },
+  {
+    id: 5,
+    text: 'To err is human, to forgive divine.',
+    author: 'Alexander Pope',
+  },
+  {
+    id: 6,
+    text: "I never met a plausible excuse I didn't like.",
+    author: 'Leslie Miklosy',
+  },
+  {
+    id: 7,
+    text: 'Words form the thread on which we string our experiences.',
+    author: 'Aldous Huxley',
+  },
+];
