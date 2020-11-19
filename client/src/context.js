5a9aeb56b5f1e360422da4c77d5ae7eefabc29a2
marvoqs <marvoqs@gmail.com>
2020-11-19 11:55:17 +0100

    get random quote from database

--- client/src/context.js
+++ client/src/context.js
@@ -1,17 +1,29 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { quotes } from './data';
+import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: './api',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [randomQuote, setRandomQuote] = useState({ id: 0, text: '', author: '' });
+  const [randomQuote, setRandomQuote] = useState({ _id: 0, text: '', author: '' });
 
-  const getRandomQuote = () => {
-    const random = Math.floor(Math.random() * quotes.length);
-    if (randomQuote.id === quotes[random].id) {
-      return getRandomQuote();
+  const getRandomQuote = async () => {
+    try {
+      const response = await axios.get('./api/quotes/random');
+      const newRandomQuote = response.data[0];
+      if (newRandomQuote._id === randomQuote._id) {
+        return getRandomQuote();
+      }
+      setRandomQuote(newRandomQuote);
+    } catch (error) {
+      console.log(error);
     }
-    setRandomQuote(quotes[random]);
   };
 
   useEffect(() => {
--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get('/', (req, res) => res.send('API Running'));
 // define quotes route
 app.use('/api/quotes', require('./routes/api/quotes'));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
